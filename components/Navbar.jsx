c3f7b7df5105ff73dd127cccbb2f2d5ac2e40a90
Assan(Jon) Bryant <krystalgame@gmail.com>
2025-07-25 15:40:34 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,7 +5,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link';
 
 
-const pages = ['Home', 'About', 'Menu', 'Services'];
+const pages = ['Home']; //, 'About', 'Menu', 'Services'
 
 
 
